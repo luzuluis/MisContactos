7040f8db2bda0c2b5064d59f8b67ddd5a3c9a07d
Luis Antonio Luzuriaga <luzuluis@hotmail.com>
2017-03-29 21:17:42 +0200

    Capitulo 10 utilizando builder para elegir varias opciones(Camara,Galeria,Document,Cancelar)

--- app/src/main/java/com/luis/espol/miscontactos/CrearContactoFragment.java
+++ app/src/main/java/com/luis/espol/miscontactos/CrearContactoFragment.java
@@ -1,12 +1,19 @@
 package com.luis.espol.miscontactos;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
 import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
@@ -22,6 +29,11 @@ import com.luis.espol.miscontactos.util.Contacto;
 import com.luis.espol.miscontactos.util.ContactoReceiver;
 import com.luis.espol.miscontactos.util.TextChangedListener;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 //import android.app.Fragment;
 //import android.support.v4.app.Fragment;
 
@@ -32,7 +44,13 @@ public class CrearContactoFragment extends Fragment implements View.OnClickListe
     private EditText txtNombre,txtTelefono,txtEmail,txtDireccion;
     private ImageView imgViewContacto;
     private Button btnAgregar;
-    private int request_code=1;
+    private final int request_code=1;
+    private final int request_select=2;
+    private String APP_DIRECTORY ="MyPicturesApp" ;
+    private String MEDIA_DIRECTORY="media";
+    private String FILE_NAME="pic_";
+    private String EXTENSION_NAME=".jpg";
+    private String rutaArchivo;
     @Override
     public void onClick(View view) {
         if(view==btnAgregar){
@@ -47,38 +65,115 @@ public class CrearContactoFragment extends Fragment implements View.OnClickListe
             btnAgregar.setEnabled(false);
             limpiarCampos();
         }else if(view==imgViewContacto){
-            Intent intent=null;
-            if(Build.VERSION.SDK_INT<19){
-                intent=new Intent();
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-            }else {
-                intent=new Intent(Intent.ACTION_OPEN_DOCUMENT);
-                intent.addCategory(Intent.CATEGORY_OPENABLE);
-            }
-            intent.setType("image/*");
-            startActivityForResult(intent,request_code);
+            final CharSequence[] opciones={"Tomar Foto","Elegir Galeria","Elegir Open Document","Cancelar"};
+            final AlertDialog.Builder dialogo=new AlertDialog.Builder(view.getContext());
+            dialogo.setTitle("Elige una opcion!!");
+            dialogo.setItems(opciones,new DialogInterface.OnClickListener(){
+                @Override
+                public void onClick(DialogInterface dialog, int seleccion) {
+                    Intent intent=null;
+                    switch (opciones[seleccion].toString()){
+                        case "Tomar Foto":
+                            activaCamara();
+                            break;
+                        case "Elegir Galeria":
+                            intent=new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                            intent.setType("image/*");
+                            //startActivityForResult(intent,request_select);
+                            startActivityForResult(intent.createChooser(intent,"Seleccione App de Imagen"),request_select);
+                            break;
+                        case "Elegir Open Document":
+                            if(Build.VERSION.SDK_INT<19){
+                                intent=new Intent();
+                                intent.setAction(Intent.ACTION_GET_CONTENT);
+                            }else {
+                                intent=new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                            }
+                            intent.setType("image/*");
+                            startActivityForResult(intent,request_select);
+                            break;
+                        default:
+                            //cierra la ventana de dialogo
+                            dialog.dismiss();
+                            break;
+                    }
+                }
+            });
+            dialogo.show();
         }
 
     }
+private void decodeBitmap(String dir){
+    Bitmap bitmap;
+    bitmap= BitmapFactory.decodeFile(dir);
+    imgViewContacto.setImageBitmap(bitmap);
+}
+    private void activaCamara() {
+        APP_DIRECTORY= String.valueOf(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES));
+        File archivo=new File(APP_DIRECTORY,MEDIA_DIRECTORY);
+        archivo.mkdir();
+        rutaArchivo=APP_DIRECTORY+File.separator+MEDIA_DIRECTORY+File.separator+getDateFormat()+EXTENSION_NAME;
+        File mi_foto=new File(rutaArchivo);
+        try {
+            mi_foto.createNewFile();
+        } catch (IOException ex) {
+            String msg="Error: "+ex;
+            Toast.makeText(getContext(),msg,Toast.LENGTH_SHORT).show();
+        }
+        //
+        Uri uri = Uri.fromFile( mi_foto );
+        //Abre la camara para tomar la foto
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //Guarda imagen
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+        //Retorna a la actividad
+        startActivityForResult(cameraIntent, request_code);
+    }
 
+    @SuppressLint("SimpleDateFormat")
+    private String getDateFormat()
+     {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyymmddhhmmss");
+        String date = dateFormat.format(new Date() );
+        //String photoCode = "pic_" + date;
+        String photoCode = FILE_NAME + date;
+        return photoCode;
+     }
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        if(resultCode== Activity.RESULT_OK && requestCode==request_code){
-            //se debe poner null para refrescar la imagen y no quede la anterior
-            imgViewContacto.setImageURI(null);
-            //obtiene y guarda la imagen en ImageView
-            imgViewContacto.setImageURI(data.getData());
-            //guarda la imagen para que no se pierda en el Tag
-            imgViewContacto.setTag(data.getData());
-            //metodo que hace redonda la imagen
-            imgViewContacto.setImageDrawable(imageRound(imgViewContacto.getDrawable()));
-            //imgViewContacto.setSize();
-
-        }else{
-           // imgViewContacto.setImageResource(R.drawable.usuario);
-            imgViewContacto.setTag("");
-
+        switch (requestCode){
+            //si se ha seleccionado de la camara
+            case request_code:
+                if(resultCode== Activity.RESULT_OK ){
+                    //se debe poner null para refrescar la imagen y no quede la anterior
+                    imgViewContacto.setImageURI(null);
+                    //obtiene y guarda la imagen en ImageView
+                    imgViewContacto.setImageURI(Uri.parse(rutaArchivo));
+                    //guarda la imagen para que no se pierda en el Tag
+                    imgViewContacto.setTag(Uri.parse(rutaArchivo));
+                    //metodo que hace redonda la imagen
+                    imgViewContacto.setImageDrawable(imageRound(imgViewContacto.getDrawable()));
+                }else{
+                 imgViewContacto.setTag("");
+                }
+                break;
+            //si se ha seleccionado de la galeria
+            case request_select:
+                if(resultCode== Activity.RESULT_OK ){
+                    //se debe poner null para refrescar la imagen y no quede la anterior
+                    imgViewContacto.setImageURI(null);
+                    //obtiene y guarda la imagen en ImageView
+                    imgViewContacto.setImageURI(data.getData());
+                    //guarda la imagen para que no se pierda en el Tag
+                    imgViewContacto.setTag(data.getData());
+                    //metodo que hace redonda la imagen
+                    imgViewContacto.setImageDrawable(imageRound(imgViewContacto.getDrawable()));
+                }else {
+                    imgViewContacto.setTag("");
+                }
+                break;
         }
     }
 
