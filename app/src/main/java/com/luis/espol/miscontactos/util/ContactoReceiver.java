b57aea9ec562e4eec5a9eb80f2357ae3099d8cf6
Luis Antonio Luzuriaga <luzuluis@hotmail.com>
2017-07-07 03:15:35 +0200

    Capitulo 10 Manejo de TelephonyManager

--- app/src/main/java/com/luis/espol/miscontactos/util/ContactoReceiver.java
+++ app/src/main/java/com/luis/espol/miscontactos/util/ContactoReceiver.java
@@ -1,16 +1,21 @@
 package com.luis.espol.miscontactos.util;
 
-import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+
+import static android.telephony.TelephonyManager.CALL_STATE_IDLE;
+import static android.telephony.TelephonyManager.CALL_STATE_OFFHOOK;
 
 //import static android.support.v4.content.ContextCompat.startActivity;
 
@@ -24,7 +29,10 @@ public class ContactoReceiver extends BroadcastReceiver {
     public static final int CONTACTO_LLAMAR=4;
     private  final ArrayAdapter<Contacto> adapterContacto;
     private Context contextActivity;
-    private final int request_code=1;
+    private static final String TAG = "PhoneStateBroadcastReceiver";
+    String incoming_number;
+    private int prev_state;
+    TelephonyManager telephony;
 
     public ContactoReceiver(ArrayAdapter<Contacto> adapter) {
         this.adapterContacto = adapter;
@@ -43,11 +51,20 @@ public class ContactoReceiver extends BroadcastReceiver {
     }
 
     private void llamarContacto(Intent intent) {
+        //TelephonyManager object
+        telephony = (TelephonyManager)contextActivity.getSystemService(Context.TELEPHONY_SERVICE);
+        CustomPhoneStateListener customPhoneListener = new CustomPhoneStateListener();
+        //Register our listener with TelephonyManager
+        telephony.listen(customPhoneListener, PhoneStateListener.LISTEN_CALL_STATE);
+
+        Bundle bundle = intent.getExtras();
+        String phoneNr = bundle.getString("incoming_number");
+        Toast.makeText(contextActivity,"telefono numero"+phoneNr,Toast.LENGTH_SHORT).show();
+
         ArrayList<Contacto> lista=(ArrayList<Contacto>) intent.getSerializableExtra("datos");
         if(lista==null){
             Toast.makeText(contextActivity,"No se puede llamar al contacto,si no esta seleccionado el item",Toast.LENGTH_SHORT).show();
         }else {
-            int contador=0;
             for (Contacto c : lista){
                 //String nombre = String.valueOf(c.getNombre());
                 //adapterContacto.remove(c);
@@ -58,39 +75,16 @@ public class ContactoReceiver extends BroadcastReceiver {
                         PackageManager.PERMISSION_GRANTED){
                     return;
                 }
-                //Retorna a la actividad
-                contextActivity.startActivity(llamadaIntent);
-                //contador++;
-                String nombre=c.getNombre();
-                String msg=String.format("%s ha colgada la llamda,hasta la proxima!!",nombre);
-                Toast.makeText(contextActivity, msg, Toast.LENGTH_SHORT).show();
-            }/*
-            if (contador==1) {
-                String msg = String.format("%s item ha sido eliminado de la lista!", contador);
-                Toast.makeText(contextActivity, msg, Toast.LENGTH_SHORT).show();
-            }else if(contador>1){
-                String msg = String.format("%s items han sido eliminados de la lista!", contador);
-                Toast.makeText(contextActivity, msg, Toast.LENGTH_SHORT).show();
-            }*/
-
-        }
-    }
-
-    //@Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        switch (requestCode){
-            //si se ha seleccionado de la camara
-            case request_code:
-                if(resultCode== Activity.RESULT_OK ){
-                    //Bundle extras=data.getExtras();
-                    //obtiene y guarda la imagen en ImageView
-                    //imgViewContacto.setImageURI(Uri.parse(rutaArchivo));
-                    //guarda la imagen para que no se pierda en el Tag
-
+                //Pregunta si esta vacio el campo telefono
+                if(c.getTelefono()==""){
+                    String nombre=c.getNombre();
+                    String msg=String.format("%s Hola no se te puede llamar porque faltan los datos de tu telefono!!",nombre);
+                    Toast.makeText(contextActivity, msg, Toast.LENGTH_SHORT).show();
+                    return;
                 }
-                break;
-
+                //Llama a la actividad que se encargara de hacer la llamada!!
+                contextActivity.startActivity(llamadaIntent);
+            }
         }
     }
 
@@ -128,5 +122,44 @@ public class ContactoReceiver extends BroadcastReceiver {
         int posicion=adapterContacto.getPosition(contacto);
         adapterContacto.insert(contacto,posicion);
 
+    }
+
+    /* Custom PhoneStateListener */
+    public class CustomPhoneStateListener extends PhoneStateListener {
+        private static final String TAG = "CustomPhoneStateListener";
+
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber){
+
+            if( incomingNumber != null && incomingNumber.length() > 0 )
+                incoming_number = incomingNumber;
+
+            switch(state){
+                case TelephonyManager.CALL_STATE_RINGING:
+                    //Log.d(TAG, "CALL_STATE_RINGING");
+                    Toast.makeText(contextActivity,TAG+" CALL_STATE_RINGING",Toast.LENGTH_SHORT).show();
+                    prev_state=state;
+                    break;
+
+                case CALL_STATE_OFFHOOK:
+                    //Log.d(TAG, "CALL_STATE_OFFHOOK");
+                    Toast.makeText(contextActivity,TAG+" CALL_STATE_OFFHOOK",Toast.LENGTH_SHORT).show();
+                    prev_state=state;
+                    break;
+
+                case CALL_STATE_IDLE:
+                    //Log.d(TAG, "CALL_STATE_IDLE==>"+incoming_number);
+                    Toast.makeText(contextActivity,TAG+"CALL_STATE_IDLE==>"+incoming_number,Toast.LENGTH_SHORT).show();
+                    if((prev_state == CALL_STATE_OFFHOOK)){
+                        prev_state=state;
+                        //Answered Call which is ended
+                    }
+                    if((prev_state == TelephonyManager.CALL_STATE_RINGING)){
+                        prev_state=state;
+                        //Rejected or Missed call
+                    }
+                    break;
+            }
+        }
     }
 }
